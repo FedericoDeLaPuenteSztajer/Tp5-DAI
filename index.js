55f6f0a65707ad0e139788fc25e203b34097f68f
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-24 12:14:10 -0300

    conexcion a API incompleta

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const input= document.getElementById("userinput");
-const button= document.getElementById("search");
+const input = document.getElementById("userinput");
+const button = document.getElementById("search");
 
-button.addEventListener("click", () =>{
-    console.log(input.value);
-})
+//https://pokeapi.co/
+const url = "https://pokeapi.co/api/v2/pokemon/";
+
+
+async function Search() {
+    try {
+        const res = await fetch(url + input.value);
+        if (!res.ok) throw new Error('HTTP ' + res.status);
+
+        const data = res.json();
+        console.log(data);
+
+    } catch (e) {
+        throw e;
+    }
+
+}
 
