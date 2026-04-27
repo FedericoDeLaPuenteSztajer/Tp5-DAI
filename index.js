eb26a66df68ae29d5111dfb69b1945934f94b224
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-27 08:19:36 -0300

    conexcion lista, carga de datos en el indexx

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const input = document.getElementById("userinput");
 const button = document.getElementById("search");
 
+const pokeName = document.getElementById("pokeName");
+const pokeFoto = document.getElementById("pokeFoto");
+const pokeTypes = document.getElementById("pokeTypes");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+
 //https://pokeapi.co/
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -9,9 +15,13 @@ async function Search() {
     try {
         const res = await fetch(url + input.value);
         if (!res.ok) throw new Error('HTTP ' + res.status);
+        const data = await res.json();
 
-        const data = res.json();
-        console.log(data);
+        pokeName.textContent= data.name;
+        pokeFoto.src= data.sprites.front_default;
+        pokeTypes.textContent= data.types[0] +"/"+ data.types[1];
+        weight.textContent= data.weight;
+        height.textContent= data.height;
 
     } catch (e) {
         throw e;
